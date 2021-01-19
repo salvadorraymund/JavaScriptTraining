68ce0af4c899fbc0248bdc1c80a91f8228d6f3ea
Raymund Salvador <raymund.martinsalvador@gmail.com>
2021-01-19 10:05:52 +0800

    January 19

---
+++ script.js
@@ -0,0 +1,22 @@
+var date = new Date();
+	document.body.innerHTML = "<h1>Today is " +date+ "</h1>"
+
+var a = 4;
+var b = "ducks";
+var sum = a + b;
+
+var humanSum = "The value of a: " + a + ". The value of b: " + b + ". The sum of a and b: " + sum + ". ";
+console. log(humanSum);
+
+var x = 5;
+var y = 4;
+var theNumbersMatch;
+
+if ( x==y ) {
+	theNumbersMatch = true;
+} else {
+	theNumbersMatch = false;
+}
+
+document.body.innerHTML = "<h1>The numbers match: " + theNumbersMatch +"</h1>"
+
