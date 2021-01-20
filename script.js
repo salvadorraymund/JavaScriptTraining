91293c497595b0114f38a6d873a773dc8589b411
Raymund Salvador <raymund.martinsalvador@gmail.com>
2021-01-20 10:32:24 +0800

    January 20

--- script.js
+++ script.js
@@ -20,3 +20,87 @@ if ( x==y ) {
 
 document.body.innerHTML = "<h1>The numbers match: " + theNumbersMatch +"</h1>"
 
+var age = 21;
+if (age >= 18) {
+	console.log("You are an adult!");
+} else {
+	console.log("You are a kid");
+}
+
+//Ternary codes
+
+// condition ? exper 1 : exper 2
+
+console.log((age >= 21) ? "You are old!" : "You are young");
+
+var stop;
+
+age > 18 ?(
+ console.log("Ok, you can go."),
+ stop = false
+) : (
+ console.log("Sorry, you are much too young!"),
+ stop = true
+ );
+
+var firstCheck = false,
+    secondCheck = true,
+    access = firstCheck ? "Access denied" : secondCheck ? "Access denied" : "Access granted";
+
+    console.log(access);
+
+
+
+//Array
+var pens;
+pens = ["red", "blue", "yellow", "orange"];
+
+console.log(pens);
+
+var firstPen = pens[1];
+
+console.log(firstPen);
+
+//Arrays and Properties
+console.log("Before: ", pens);
+
+//PROPERTIES
+//Get a property of an object by name:
+// console.log("Array length: ", pens.length);
+
+//Methods
+//Reverse the array:
+// pens.reverse ();
+
+//Remove the first value of the array
+// pens.shift();
+
+//Add comma-separated list of values to the front of the array
+// pens.unshift("purple", "black");
+
+//Remove the last value of the array:
+//pens.pop();
+
+// var newPens = pens.slice();
+// console.log("New pens: ", newPens);
+
+// console.log("After: ", pens);
+
+var arrayString = pens.join(", ");
+console.log("String from array: ", arrayString);
+
+
+var number1 = 40;
+var number2 = 51;
+var sum1 = number1 + number2;
+
+if ((number1 == 100 || number2 == 100) || (sum1 >= 100)){
+	console.log("true");
+} else {
+	console.log("false");
+};
+
+
+
+
+ 
